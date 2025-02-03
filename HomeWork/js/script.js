b57bd7670b11c6825ca99d03a7f2fd858f5c8d44
Tetiana Ihnatenko <ti.licey@gmail.com>
2025-02-03 21:10:21 +0200

    add solution

--- HomeWork/js/script.js
+++ HomeWork/js/script.js
@@ -5,35 +5,47 @@
 // Скопіюйте значення зі змінної name в city.
 // Виведіть значення змінної city, використовуючи функцію console.log (яка повинна показати “Іван”).
 
+// let name = "Іван";
+// let city = name;
+// console.log(city);  // Іван
+
 //***2***
-//Який буде результат виконання скрипта?
+// Який буде результат виконання скрипта?
 // let name = "Olga";
-// console.log(`привіт ${1}`); //
-// console.log(`привіт ${"name"}`); //
-// console.log(`привіт ${name}`); // ?
+// console.log(`привіт ${1}`);      // привіт 1
+// console.log(`привіт ${"name"}`); // привіт name
+// console.log(`привіт ${name}`);   // привіт Olga
 
 //***3***
 //Видобути число зі змінних
 // let a = "5";
 // let b = "13cvb";
 // let c = "12.9sxdcfgv";
+// console.log(typeof Number(a));   // number
+// console.log(typeof Number(b));   // number
+// console.log(typeof Number(c));   // number
 // вивести в консоль тип
 
 //***4***
 //Зробіть, щоб 0.1 + 0.2 = 0.3
+// console.log((0.1*10 + 0.2*10)/10);   // 0.3
 
 //***5**
 //Поверніть найбільше число с набору 20, 10, 50, 40
+// console.log(Math.max(20, 10, 50, 40));   // 50
 
 //***6**
 //Поверніть випадкове число в діапазоні від 2 до 4
+// console.log(Math.random() * (4 - 2) + 2);   // випадкове число від 2 до 4
 
 //***7**
 //дізнатись довжину message
 // const message = "Welcome to Bahamas!";
+// console.log(message.length);   // 19
 
 //***8**
 //вивести в консоль message  великими літерами
+// console.log(message.toUpperCase());   // WELCOME TO BAHAMAS!
 
 //***9**
 // створити пустий об*єкт
@@ -43,5 +55,19 @@
 // додати буль з ключем: like flowers
 // вивести результат в консоль
 
-//***10**
-// За допомогою циклу  “for…in” вивести в консоль ключі і значення об*єкта
+// let obj = {};
+// obj.name = "Tetiana";
+// obj.age = 55;
+// obj.city = "Nizhyn";
+// console.log(obj);        // {name: "Tetiana", age: 55, city: "Nizhyn"}
+// delete obj.city;
+// obj["like flowers"] = true;
+// console.log(obj);        // {name: "Tetiana", age: 55, like flowers: true}
+
+// //***10**
+// // За допомогою циклу  “for…in” вивести в консоль ключі і значення об*єкта
+
+// for (let key in obj) {
+//     console.log(key)    ;   // name Tetiana age 55 like flowers true
+//     console.log(obj[key]);  
+// }
